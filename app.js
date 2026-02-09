5c1707a68c2ba5314c029df6e41d77b92d71523a
Bisma Raza <bismauniversity@gmail.com>
2026-02-09 20:27:41 +0500

    String Methods

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-let msg = "Hello Bisma How Are You?  ";
+let name = "ApnaCollege";
+let newname = name.slice(4);
+console.log(newname);
 
-// let newmsg= msg.trim();
-// newmsg = newmsg.toUpperCase();
-
-let newmsg = msg.trim().toUpperCase();
-console.log(newmsg);
-
-console.log(newmsg.slice(6,11))
+newname = newname.replace("ll", "tt");
+console.log(newname);
