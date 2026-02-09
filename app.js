bb5bc0d2867b6b9b3aec7e39ea530fa49a2f01ba
Bisma Raza <bismauniversity@gmail.com>
2026-02-09 20:00:02 +0500

    Update APP.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let num1 = 32;
-let num2 = 47852;
+let msg = "Hello Bisma How Are You?  ";
 
-if( (num1%10) == (num2%10) ) {
-    console.log("The number ends with 2");
-}
-else {
-    console.log("The number does not end with 2");
-}
+// let newmsg= msg.trim();
+// newmsg = newmsg.toUpperCase();
+
+let newmsg = msg.trim().toUpperCase();
+console.log(newmsg);
+
+console.log(newmsg.slice(6,11))
