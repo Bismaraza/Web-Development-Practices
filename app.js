061bde888ed79c53f7ff0c8ab3a0901f613bc6b7
Bisma Raza <bismauniversity@gmail.com>
2026-02-13 20:52:04 +0500

    Next Array

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-let cars = [" Audi", "BMW", "XUV"];
+let str = prompt("Enter yout string: ");
+
+if(str.length == 0)
+{
+    console.log(" String is empty")
+}
+else{
+    console.log("not empty");
+}
