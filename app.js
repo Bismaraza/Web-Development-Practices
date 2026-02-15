13135acd730ae3e48ddc8227e9ac973af875f907
Bisma Raza <bismauniversity@gmail.com>
2026-02-15 21:18:54 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 
-// Check the element is Lower case 
-let name = "BiSMA";
-let index= 1;
+// // Check the element is Lower case 
+// let name = "BiSMA";
+// let index= 1;
 
-if(name[1] == name[1].toLowerCase())
-{
-    console.log(" Lower ");
-}
-else{
-    console.log("Now lower");
-}
+// if(name[1] == name[1].toLowerCase())
+// {
+//     console.log(" Lower ");
+// }
+// else{
+//     console.log("Now lower");
+// }
 
-// Iten exists in array
-let arr =[ "BISMA", "R", 34,56,23,"RAZA"];
-let check = "R";
+// // Iten exists in array
+// let arr =[ "BISMA", "R", 34,56,23,"RAZA"];
+// let check = "R";
 
-if (arr.indexOf(check) != -1)
-{
-    console.log("Yes");
-}
-else {
-    console.log("No");
-}
+// if (arr.indexOf(check) != -1)
+// {
+//     console.log("Yes");
+// }
+// else {
+//     console.log("No");
+// }
 
-// Trim Function
+// // Trim Function
 
-let str = "        BISMA       ";
-console.log(str);
-console.log(str.trim());
+// let str = "        BISMA       ";
+// console.log(str);
+// console.log(str.trim());
