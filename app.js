602bb1c00888cd08842cb116630d17a749f26069
Bisma Raza <bismauniversity@gmail.com>
2026-02-15 22:33:03 +0500

    Update app.js

--- app.js
+++ app.js
@@ -29,3 +29,31 @@
 // console.log(str);
 // console.log(str.trim());
 
+
+// // Loops
+
+// for (let i = 3; i<=30; i+=3)
+//     console.log(i)
+
+// for (let i= 1; i<= 3; i++)
+// {
+//     for(let i = 1; i<= 3; i++)
+//     {
+//         console.log(i)
+//     }
+// }
+
+// Guess
+
+const favMovie = "avatar";
+
+let guess = prompt("Guess My Fav Moview");
+
+while ((guess != favMovie) && (guess != "quit")) {
+    guess = prompt("Wrong Guess, Try AGain");
+}
+if (guess == favMovie)
+    console.log("yes true");
+else{
+    console.log(" You quit")
+}
