47a282e4608015d5b9175214adeab40805e98bfa
Bisma Raza <bismauniversity@gmail.com>
2026-02-19 17:02:51 +0500

    Web Development

--- guessing_number.js
+++ guessing_number.js
@@ -1,26 +1,37 @@
-const max = prompt(" Enter a max number");
-const random = Math.floor (Math.random() * max ) + 1;
+// const max = 6;
+// const random = Math.floor (Math.random() * 6 ) + 1 ;
 
 
-let guess = prompt(" Guess the number: ");
-while(true)
-{
-    if(guess == "quit"){
-        console.log("User Quit");
-        break;
-    }
+// let guess = prompt(" Guess the a number: ");
+// while(true)
+// {
+//     if(guess == "quit"){
+//         console.log("User Quit");
+//         break;
+//     }
 
-    if(guess == random){
-        console.log(" congrats! Random Number was " + random);
-        break;
-    }
-    else if( guess < random)
-    {
-        guess = prompt(" Hint: your guess was too small. Try again")
-    }
+//     if(guess == random){
+//         console.log(" congrats! Random Number was " + random);
+//         break;
+//     }
+//     else if( guess < random)
+//     {
+//         guess = prompt(" Hint: your guess was too small. Try again");
+//     }
 
-    else {
-        guess = prompt("Hint:  Your guess was too large, please try again");
-    }
+//     else {
+//         guess = prompt("Hint:  Your guess was too large, please try again");
+//     }
+// }
+// console.log(random);
+
+const Person = {
+    name : "Bisma Raza",
+    age: 20,
+    City: "Mianwali"
 }
-console.log(random);
+
+Person.city = " NewYork";
+
+Person.country = " Pak";
+console.log(Person);
