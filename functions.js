dd1bd7552f0fe962abeacb5a8ed2903109a40e14
Bisma Raza <bismauniversity@gmail.com>
2026-02-20 16:02:59 +0500

    Return.

--- functions.js
+++ functions.js
@@ -1,7 +1,18 @@
-function print1to5(){
-    for(let i = 1; i<=5; i++){
-        console.log(i);
-    }
+function dice(){
+    const random = Math.floor (Math.random() * 6 ) + 1 ;
+    console.log(random);
 }
 
-print1to5();
+// dice();
+
+function table(n){
+    for(let i = 1; i<=10; i++){
+    let mul = n*i;
+    console.log(mul);
+    }
+}
+// table(73);
+
+let arr = [ 1,2,3,4];
+let part = arr.slice(2,4);
+console.log(part);
--- guessing_number.js
+++ guessing_number.js
@@ -1,29 +1,29 @@
-// const max = 6;
-// const random = Math.floor (Math.random() * 6 ) + 1 ;
+const max = 6;
+const random = Math.floor (Math.random() * 6 ) + 1 ;
 
 
-// let guess = prompt(" Guess the a number: ");
-// while(true)
-// {
-//     if(guess == "quit"){
-//         console.log("User Quit");
-//         break;
-//     }
+let guess = prompt(" Guess the a number: ");
+while(true)
+{
+    if(guess == "quit"){
+        console.log("User Quit");
+        break;
+    }
 
-//     if(guess == random){
-//         console.log(" congrats! Random Number was " + random);
-//         break;
-//     }
-//     else if( guess < random)
-//     {
-//         guess = prompt(" Hint: your guess was too small. Try again");
-//     }
+    if(guess == random){
+        console.log(" congrats! Random Number was " + random);
+        break;
+    }
+    else if( guess < random)
+    {
+        guess = prompt(" Hint: your guess was too small. Try again");
+    }
 
-//     else {
-//         guess = prompt("Hint:  Your guess was too large, please try again");
-//     }
-// }
-// console.log(random);
+    else {
+        guess = prompt("Hint:  Your guess was too large, please try again");
+    }
+}
+console.log(random);
 
 const Person = {
     name : "Bisma Raza",
