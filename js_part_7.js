6f440d0f1b295578fb4b22e69a79cde71a88648f
Bisma Raza <bismauniversity@gmail.com>
2026-02-22 15:59:59 +0500

    Update js_part_7.js

--- js_part_7.js
+++ js_part_7.js
@@ -1,11 +1,26 @@
 
 // Arrow Function
-const sum = (a, b) => {
-    console.log(a + b);
-}
-const cube=(n)=> {
-    return n*n*n;
-}
+// const sum = (a, b) => {
+//     console.log(a + b);
+// }
+// const cube=(n)=> {
+//     return n*n*n;
+// }
 
-const pow = (a,b) =>(
-a*b)
+// const pow = (a,b) =>(
+// a*b)
+
+// Set timeout
+
+console.log("Hi there");
+setTimeout(() =>{
+    console.log("Apna college")
+}, 4000);
+
+console.log("Welcome to");
+
+console.log("Hi there");
+let id = setInterval(() =>{
+    console.log("Apna college")
+}, 2000);
+console.log(id);
