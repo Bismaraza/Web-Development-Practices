2d4abf044271eddbf832f90456685da3c4964e7d
Bisma Raza <bismauniversity@gmail.com>
2026-02-25 20:01:04 +0500

    Update jspart8.js

--- jspart8.js
+++ jspart8.js
@@ -38,3 +38,9 @@ let gpa = arrr.map((el) => {
 arrr.forEach((student) =>{
     console.log(student.marks);
 });
+
+let nums = [1,2,3,4,5,6,3,2,5,6];
+let ans = nums.filter((el) => {
+    return el<5;
+});
+
